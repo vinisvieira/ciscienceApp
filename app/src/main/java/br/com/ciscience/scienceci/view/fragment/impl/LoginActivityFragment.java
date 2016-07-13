5c15e8c7fd87455ba22f2aaf535cc13838f18a63
pedrodimoura <pedro.moura@fafica-pe.edu.br>
2016-07-13 18:51:44 -0300

    Tela de Questões e Alternativas parcialmente finalizada

--- app/src/main/java/br/com/ciscience/scienceci/view/fragment/impl/LoginActivityFragment.java
+++ app/src/main/java/br/com/ciscience/scienceci/view/fragment/impl/LoginActivityFragment.java
@@ -1,22 +1,30 @@
 package br.com.ciscience.scienceci.view.fragment.impl;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import br.com.ciscience.scienceci.R;
+import br.com.ciscience.scienceci.view.activity.impl.MainActivity;
 import butterknife.BindString;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class LoginActivityFragment extends Fragment {
+public class LoginActivityFragment extends Fragment implements View.OnClickListener {
 
     @BindString(R.string.debug_key) String debugKey;
+
+    @BindView(R.id.relativeLayoutLogin) RelativeLayout relativeLayoutLogin;
+    @BindView(R.id.relativeLayoutLoginSplash) RelativeLayout relativeLayoutLoginSplash;
 
     private View mView;
 
@@ -31,5 +39,12 @@ public class LoginActivityFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @OnClick({R.id.buttonLogin})
+    @Override
+    public void onClick(View view) {
+        getActivity().finish();
+        startActivity(new Intent(getActivity(), MainActivity.class));
     }
 }
